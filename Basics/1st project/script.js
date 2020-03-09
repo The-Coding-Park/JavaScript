2f0471d2f4fa50b1572f9958de0b2a5d148c3cd7
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-03-09 19:51:58 +0530

    new code

---
+++ Basics/1st project/script.js
@@ -0,0 +1,17 @@
+console.log("hello world 2");
+
+
+// variabl
+var firstname="Gunjan";
+var age=20;
+var college="MIT";
+var ok=true;
+console.log(firstname);
+console.log(age);
+console.log(college);
+console.log(age);
+
+
+var job;
+console.log(job); //undefined
+
